82a5046645132269b59eba45fb4980a6a5c280b6
chusta <chusta@users.noreply.github.com>
2017-03-24 04:14:27 -0400

    create test configuration

--- pasta/config.py
+++ pasta/config.py
@@ -11,14 +11,25 @@ with open(DB_LOGIN, "rb") as f:
     username = j["username"]
     password = j["password"]
 
+class TestConfig(object):
+    """
+    test configuration
+    """
+    SQLALCHEMY_DATABASE_URI = "postgresql://{}:{}@127.0.0.1:5432/pasta-test".format(username, password)
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    DEBUG = True
+    SECRET_KEY = os.environ.get("SECRET_KEY", "")
+    MAX_CONTENT_LENGTH = 5 * 1024 * 1024 # 5MB
+    PAGINATION = 10
+
 
 class PastaConfig(object):
     """
-    postgresql configuration
+    pasta configuration
     """
     SQLALCHEMY_DATABASE_URI = "postgresql://{}:{}@127.0.0.1:5432/pasta".format(username, password)
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    DEBUG = True
+    DEBUG = False
     SECRET_KEY = os.environ.get("SECRET_KEY", "")
     MAX_CONTENT_LENGTH = 5 * 1024 * 1024 # 5MB
     PAGINATION = 10
